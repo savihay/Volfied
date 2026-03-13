27d33fdb9b3b8dc2c0e7355d8790d009b8829ce4
Avihay Barazany <savihay@gmail.com>
2026-03-13 12:18:07 +0200

    Initialize Vite and React tooling for GH Pages deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Volfied from './volfied'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Volfied />
+  </React.StrictMode>,
+)
